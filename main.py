ae2ef71d3b39642d3589eb42540aed4dbee4626e
aaron <aaronsmitstc@gmail.com>
2023-07-29 15:13:18 -0400

    stand up additional endpoints

--- main.py
+++ main.py
@@ -1,45 +1,78 @@
+from datetime import datetime, timedelta
 from typing import Annotated
 
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 from pydantic import BaseModel
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "56d61524a9e0d7190cdfc765eaf1e0b35b6395e9c74cc294644fffce92d96ca1"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 15
+
 
 fake_users_db = {
     "johndoe": {
+        "id": 1,
         "username": "johndoe",
         "full_name": "John Doe",
         "email": "johndoe@example.com",
-        "hashed_password": "fakehashedsecret",
+        "account_number": 1234567890,
+        "balance": 1000,
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
         "disabled": False,
     },
-    "alice": {
-        "username": "alice",
-        "full_name": "Alice Wonderson",
-        "email": "alice@example.com",
-        "hashed_password": "fakehashedsecret2",
-        "disabled": True,
+    "janedoe": {
+        "id": 1,
+        "username": "janedoe",
+        "full_name": "Jane Doe",
+        "email": "jjanedoe@example.com",
+        "account_number": 9876543021,
+        "balance": 900,
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
     },
 }
 
-app = FastAPI()
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 
-def fake_hash_password(password: str):
-    return "fakehashed" + password
-
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+class TokenData(BaseModel):
+    username: str | None = None
 
 
 class User(BaseModel):
     username: str
     email: str | None = None
     full_name: str | None = None
+    account_number: int | None = None
+    balance: int | None = None
     disabled: bool | None = None
 
 
 class UserInDB(User):
     hashed_password: str
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
 
 
 def get_user(db, username: str):
@@ -48,21 +81,43 @@ def get_user(db, username: str):
         return UserInDB(**user_dict)
 
 
-def fake_decode_token(token):
-    # This doesn't provide any security at all
-    # Check the next version
-    user = get_user(fake_users_db, token)
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
     return user
 
 
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
     return user
 
 
@@ -74,50 +129,101 @@ async def get_current_active_user(
     return current_user
 
 
-@app.post("/token")
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-
-    return {"access_token": user.username, "token_type": "bearer"}
+async def get_all_users():
+    return fake_users_db
 
 
-@app.get("/users/me")
+@app.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/accounts/me", response_model=User)
 async def read_users_me(
     current_user: Annotated[User, Depends(get_current_active_user)]
 ):
     return current_user
 
-# @app.post("/accounts")
-# async def create_account():
-#     return {"message": "Hello World"}
+
+@app.get("/accounts")
+async def get_accounts():
+    return await get_all_users()
 
 
-# @app.get("/accounts/{account_id}")
-# async def get_account(account_id: int):
-#     return {"message": "Hello World"}
+@app.post("/accounts")
+async def create_account(user: User):
+    fake_users_db.update({user.username: user.dict()})
+    return {"message": "user created successfully", "user": user.dict()}
 
 
-# @app.put("/accounts/{account_id}")
-# async def update_account(account_id: int):
-#     return {"message": "Hello World"}
+@app.get("/accounts/{account_id}")
+async def get_account(account_id: int):
+    user = get_user(fake_users_db, account_id)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
+    return user
 
 
-# @app.delete("/accounts/{account_id}")
-# async def delete_account(account_id: int):
-#     return {"message": "Hello World"}
+@app.put("/accounts/{account_id}")
+async def update_account(account_id: int, user: User):
+    user = get_user(fake_users_db, account_id)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
+    fake_users_db.update({user.username: user.dict()})
+    return {"message": "user updated successfully", "user": user.dict()}
 
 
-# @app.post("/transfer")
-# async def transfer():
-#     return {"message": "Hello World"}
+@app.delete("/accounts/{account_id}")
+async def delete_account(account_id: int):
+    user = get_user(fake_users_db, account_id)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
+    fake_users_db.pop(user.username)
+    return {"message": "user deleted successfully", "user": user.dict()}
 
 
-# @app.get("/login")
-# async def login():
-#     return {"message": "Hello World"}
+@app.post("/transfer")
+async def transfer(to_account: int, from_account: int, amount: int):
+    to_user = get_user(fake_users_db, to_account)
+    from_user = get_user(fake_users_db, from_account)
+    if not to_user or not from_user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
+    if from_user.balance < amount:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Insufficient funds",
+        )
+    to_user.balance += amount
+    from_user.balance -= amount
+    fake_users_db.update({to_user.username: to_user.dict()})
+    fake_users_db.update({from_user.username: from_user.dict()})
+    return {
+        "message": "transfer successful",
+        "from_user": from_user.dict(),
+        "to_user": to_user.dict(),
+    }
